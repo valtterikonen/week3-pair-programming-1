d8855b113ef4361c0d894584bcd7297427aae66b
Samu <samupie@metropolia.fi>
2024-01-31 14:59:44 +0200

    acivity2

---
+++ starter/src/components/Footer.js
@@ -0,0 +1,24 @@
+import PageLinks from "./PageLinks";
+function Footer() {
+  return (
+    <div>
+      <footer className="section footer">
+      <PageLinks parentClass='footer-links' itemClass='footer-link' />
+            <ul className="footer-icons">
+                <li>
+                    <a href="https://www.facebook.com" target="_blank" rel="noreferrer" className="footer-icon"><i className="fab fa-facebook"></i></a>
+                </li>
+                <li>
+                    <a href="https://www.twitter.com" target="_blank" rel="noreferrer" className="footer-icon"><i className="fab fa-twitter"></i></a>
+                </li>
+                <li>
+                    <a href="https://www.squarespace.com/" target="_blank" rel="noreferrer" className="footer-icon"><i className="fab fa-squarespace"></i></a>
+                </li>
+            </ul>
+            <p className="copyright">copyright © travel tours company<span id="date">2023</span>. all rights reserved</p>
+        </footer>
+    </div>
+  );
+}
+
+export default Footer;
